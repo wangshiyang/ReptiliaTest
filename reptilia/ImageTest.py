ad77305e6f279185a0731a2553669836021aa057
wangshiyang <wap521180@163.com>
2012-12-21 15:48:08 +0800

    2

---
+++ reptilia/ImageTest.py
@@ -0,0 +1,42 @@
+#encoding=gbk
+'''
+Created on 2012-12-18
+
+@author: virusyang
+'''
+
+from PIL import Image, ImageFilter, ImageEnhance
+
+image_name = "E:/Downloads/captcha1.jpg"
+
+im = Image.open(image_name,'r')
+
+im = im.filter(ImageFilter.MaxFilter)
+#
+enhancer = ImageEnhance.Contrast(im)
+
+im = enhancer.enhance(2)
+#
+#im = im.convert('P')
+#
+#im.show()
+#print im.format, im.mode, im.size 
+
+#Lim = im
+Lim = im.convert('L')  
+#Lim.save('E:/Downloads/captchaxx.jpg')  
+threshold = 80 
+table = []  
+
+for i in range(256):  
+    if i < threshold: 
+        table.append(0)  
+    else:  
+        table.append(1)  
+
+bim = Lim.point(table, '1')  
+
+bim.show()
+##
+#Lim.save('captchaxxx.jpg')
+# 
